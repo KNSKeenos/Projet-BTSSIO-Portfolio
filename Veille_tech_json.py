50126fa961a61a6f0e379f92f15bb9176f5dae2a
KNSKeenos <lucasoman195@gmail.com>
2026-06-18 12:01:07 +0200

    Update Veille_tech_json.py

--- Veille_tech_json.py
+++ Veille_tech_json.py
@@ -4,25 +4,25 @@ import json
 
 def extraire_image(article, nom_site):
     """Tente de trouver une image dans l'article, sinon renvoie une image par défaut."""
-    # 1. Tentative via les balises média de feedparser
+    # balises média de feedparser
     if 'media_content' in article and len(article.media_content) > 0:
         return article.media_content[0].get('url')
         
-    # 2. Tentative via les fichiers joints (enclosures)
+    #fichiers joints (enclosures)
     if 'enclosures' in article and len(article.enclosures) > 0:
         for enc in article.enclosures:
             if enc.get('type', '').startswith('image/'):
                 return enc.get('url')
                 
-    # 3. Image de secours (Unsplash libre de droits) adaptée au thème si aucune image n'est trouvée
+    # Image de secours si image du site non trouvé
     images_secours = {
-        "CERT-FR": "https://images.unsplash.com/photo-1563986768609-322da13575f3?w=400", # Sécurité / Cyber
-        "Sécurité Debian": "https://images.unsplash.com/photo-1629654297299-c8506221ca97?w=400", # Linux / Serveur
-        "IT-Connect": "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?w=400", # Réseau / Infra
+        "CERT-FR": "https://images.unsplash.com/photo-1563986768609-322da13575f3?w=400", 
+        "Sécurité Debian": "https://images.unsplash.com/photo-1629654297299-c8506221ca97?w=400", 
+        "IT-Connect": "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?w=400", 
     }
-    # Renvoie l'image associée ou une image tech générique
+    # Renvoie image secours si image site non trouvé
     return images_secours.get(nom_site, "https://images.unsplash.com/photo-1518770660439-4636190af475?w=400")
-
+    #flux rss des sites (peut être ajouté, ne pas oublié , : et ""
 def generer_json_veille():
     sources_rss = {
         "IT-Connect": "https://www.it-connect.fr/feed/",
@@ -48,7 +48,7 @@ def generer_json_veille():
             date_brute = article.get('published_parsed') or article.get('updated_parsed')
             date_pub = datetime(*date_brute[:6]).strftime("%d/%m/%Y") if date_brute else "Date inconnue"
             
-            # Extraction de l'image grâce à notre nouvelle fonction
+            # Extraction de l'image de l'article si trouvé
             image_url = extraire_image(article, nom_site)
                 
             donnees_site_web[nom_site].append({
